6ca88779132bed7fae0712d9c6cfd87d5c2447e4
eslaweg <eslaweg@5CG6292RR4.ericsson.se>
2016-10-31 15:28:11 +0100

    ds

--- hello/src/kurs/inheritance.java
+++ hello/src/kurs/inheritance.java
@@ -5,47 +5,40 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-
 public class inheritance {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		Punkt3D wspol = new Punkt3D();
-		
-		int odczyt[] = {-1,-1,-1};
+
+		int odczyt[] = { -1, -1, -1 };
 		boolean ifTrue = false;
-				
-		for (int j = 0; j < 3 ; j++) {
+
+		for (int j = 0; j < 3; j++) {
 			ifTrue = false;
-			
-			while (!ifTrue){
+
+			while (!ifTrue) {
 				try {
-					System.out.print("Podaj wsp " + j +": ");
+					System.out.print("Podaj wsp " + j + ": ");
 					odczyt[j] = Integer.parseInt(in.readLine());
-				}
-				catch (NumberFormatException e){
+				} catch (NumberFormatException e) {
 					System.out.println(" Wrong data" + e);
-				}
-				catch (IOException n){
+				} catch (IOException n) {
 					System.out.println("Blad odczytu");
 				}
-				ifTrue = odczyt[j] == -1? false : true;
+				ifTrue = odczyt[j] == -1 ? false : true;
 			}
 		}
-		
-		
+
 		wspol.setWspXYZ(odczyt[0], odczyt[1], odczyt[2]);
-		
+
 		System.out.print("Wsp to: ");
-		
-		
-		for (int i : wspol.wsp){
-		System.out.print(" "+ i );
-		
+
+		for (int i : wspol.wsp) {
+			System.out.print(" " + i);
+
 		}
-		
-		
 
 	}
 
