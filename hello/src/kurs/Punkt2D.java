6595fbcfeace736db4c3c754dccbcfe2f423ec7b
eslaweg <eslaweg@5CG6292RR4.ericsson.se>
2016-10-31 15:36:22 +0100

    tes

--- hello/src/kurs/Punkt2D.java
+++ hello/src/kurs/Punkt2D.java
@@ -3,29 +3,25 @@ package kurs;
 public class Punkt2D {
 
 	public int wsp[] = new int[3];
-	
-	
-	
+
 	public Punkt2D() {
 		wsp[0] = -1;
 		wsp[1] = -1;
 	}
 
-	public Punkt2D(int wspX, int wspY)
-	{
+	public Punkt2D(int wspX, int wspY) {
 		this.wsp[0] = wspX;
 		this.wsp[1] = wspY;
 	}
-	
-	public void setWspXY(int wspX, int wspY){
+
+	public void setWspXY(int wspX, int wspY) {
 		this.wsp[0] = wspX;
 		this.wsp[1] = wspY;
 	}
-	
-	public int[] getPoint(){
-		
+
+	public int[] getPoint() {
+
 		return wsp;
 	}
-	
-	
+
 }
